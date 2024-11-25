62f71f52e82042be0553f3f03098c9b76b99a0ec
Sussyamogus2016 <Naratise@gmail.com>
2024-11-25 22:05:41 +0300

    com3

--- zadanie1.py
+++ zadanie1.py
@@ -5,12 +5,4 @@ while st == '':
         print('Вы ввели пустую строку')
 
 for i in range(len(st)):
-    count_open = count_close = 0
-    if st[i] == '(':
-        count_open += 1
-        ind_open = i
-    if st[i] == ')':
-        count_close += 1
-        ind_close = i
-    if count_open == count_close == 1:
-        st = st[0:ind_open]
+    if st[i] =
