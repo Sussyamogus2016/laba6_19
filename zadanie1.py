429a843a71fadb1d9e042953281105f3c1f2af6b
Sussyamogus2016 <Naratise@gmail.com>
2024-11-25 22:22:19 +0300

    com4

--- zadanie1.py
+++ zadanie1.py
@@ -5,4 +5,9 @@ while st == '':
         print('Вы ввели пустую строку')
 
 for i in range(len(st)):
-    if st[i]
+    if st[i] == '(':
+        for j in range(i, len(st)):
+            if st[j] == ')':
+                for k in range(0, )
+
+
